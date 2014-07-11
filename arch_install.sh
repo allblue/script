51a87a255c18f6bbcb2ec64393e7fd3b08faf17d
allblue <phrc001@163.com>
2014-07-11 12:50:58 +0800

    a2

--- arch_install.sh
+++ arch_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NETWORK="firefox firefox-i18n-zh-cn thunderbird thunderbird-i18n-zh-cn  skype pidgin pidgin-lwqq "
-MEDIA="smplayer smplayer-themes vlc flashplugin  mcomix graphviz gimp "
+MEDIA="smplayer smplayer-themes vlc flashplugin  mcomix graphviz gimp feh mtpaint "
 OFFICE="gvim wiznote goldendict mupdf  "
-TOOL="wireshark-gtk  doublecmd-gtk2 fcron mlocate"
-SYSTEM="fcitx fuse ntfs-3g rsync unrar axel git  "
+TOOL="wireshark-gtk  doublecmd-gtk2 fcron mlocate lilyterm"
+SYSTEM="fcitx fuse ntfs-3g rsync unrar axel git wine wine_gecko wqy-zenhei wqy-microhei os-prober  pavucontrol"
 LOCAL="kingsoft-office metasploit  pcmanx-gtk2 gns3 fcitx-sogoupinyin"
 pacman -S -y $NETWORK $MEDIA $OFFICE $TOOL $SYSTEM $LOCAL
